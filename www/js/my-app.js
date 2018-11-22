545066d6ca69ca6fe971a94ff89358ebc376db13
cosimodivi <cosimodivi@gmail.com>
2018-11-22 16:17:11 +0100

    Add all project Cordova

---
+++ www/js/my-app.js
@@ -0,0 +1,80 @@
+// Initialize app
+var isAndroid = Framework7.prototype.device.android === true;
+var isIos = Framework7.prototype.device.ios === true;
+
+Template7.global = {
+    android: isAndroid,
+    ios: isIos
+};
+// If we need to use custom DOM library, let's save it to $$ variable:
+var $$ = Dom7;
+if (isAndroid) {
+    $$('head').append(
+        '<link rel="stylesheet" href="lib/framework7/css/framework7.material.min.css">' +
+        '<link rel="stylesheet" href="lib/framework7/css/framework7.material.colors.min.css">' +
+        '<link rel="stylesheet" href="lib/framework7/css/my-app.material.css">'
+    );
+}
+else {
+    $$('head').append(
+        '<link rel="stylesheet" href="lib/framework7/css/framework7.ios.min.css">' +
+        '<link rel="stylesheet" href="lib/framework7/css/framework7.ios.colors.min.css">' +
+        '<link rel="stylesheet" href="lib/framework7/css/my-app.ios.css">'
+    );
+}      
+
+if (isAndroid) {
+    // Change class
+    $$('.view.navbar-through').removeClass('navbar-through').addClass('navbar-fixed');
+    // And move Navbar into Page
+    $$('.view .navbar').prependTo('.view .page');
+}
+var myApp = new Framework7({ // INitialize 
+    // Enable Material theme for Android device only
+    material: isAndroid ? true : false,
+    // Enable Template7 pages
+    template7Pages: true
+});
+ 
+// Init View
+var mainView = myApp.addView('.view-main', {
+    // Material doesn't support it but don't worry about it
+    // F7 will ignore it for Material theme
+    dynamicNavbar: true
+});
+// Add view
+var mainView = myApp.addView('.view-main', {
+    // Because we want to use dynamic navbar, we need to enable it for this view:
+    dynamicNavbar: true
+});
+
+// Handle Cordova Device Ready Event
+$$(document).on('deviceready', function() {
+    console.log("Device is ready!");
+});
+
+
+// Now we need to run the code that will be executed only for About page.
+
+// Option 1. Using page callback for page (for "about" page in this case) (recommended way):
+myApp.onPageInit('about', function (page) {
+    // Do something here for "about" page
+
+})
+
+// Option 2. Using one 'pageInit' event handler for all pages:
+$$(document).on('pageInit', function (e) {
+    // Get page data from event data
+    var page = e.detail.page;
+
+    if (page.name === 'about') {
+        // Following code will be executed for page with data-page attribute equal to "about"
+        myApp.alert('Here comes About page');
+    }
+})
+
+// Option 2. Using live 'pageInit' event handlers for each page
+$$(document).on('pageInit', '.page[data-page="about"]', function (e) {
+    // Following code will be executed for page with data-page attribute equal to "about"
+    myApp.alert('Here comes About page');
+})
